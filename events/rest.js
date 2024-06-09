2631139f079f0772fec23f43e4fd949c5fbc2ec3
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-09 11:02:26 +0900

    Create rest.js

---
+++ events/rest.js
@@ -0,0 +1,30 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+require('dotenv').config();
+
+
+const commands = [];
+const commandFiles = fs.readdirSync(path.join(__dirname, '..', 'commands')).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`../commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log(`${commands.length} 個のコマンドを登録します`);
+
+        await rest.put(
+            Routes.applicationCommands(process.env.clientID),
+            { body: commands },
+        );
+
+        console.log(`${commands.length} 個のコマンドを登録しました`);
+    } catch (error) {
+        console.error(error);
+    }
+})();
