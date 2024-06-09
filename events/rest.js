0f6f80b614091a047cb36bbe2f7276294dca9411
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-09 11:38:27 +0900

    Update rest.js

--- events/rest.js
+++ events/rest.js
@@ -12,14 +12,14 @@ for (const file of commandFiles) {
     commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
     try {
         console.log(`${commands.length} 個のコマンドを登録します`);
 
         await rest.put(
-            Routes.applicationCommands(process.env.clientID),
+            Routes.applicationCommands(process.env.CLIENT_ID),
             { body: commands },
         );
 
